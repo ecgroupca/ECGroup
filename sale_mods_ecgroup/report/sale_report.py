cea8b449a3a8a7a42b5fc9ee7ce0af0267837aec
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2020-12-03 12:11:00 -0800

    final fixes and changes for deposit total functionality with computed field that sums up all deposits on sale order lines

--- sale_mods_ecgroup/report/sale_report.py
+++ sale_mods_ecgroup/report/sale_report.py
@@ -3,7 +3,7 @@
 
 import logging
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
@@ -15,17 +15,18 @@ class SaleOrder(models.Model):
         compute="_compute_deps_total",
         )
     
+    @api.depends('order_line')
     def _compute_deps_total(self):
         for sale in self:
             total_deps = 0
-            company_id = sale.company_id
-            config = self.env['res.config.settings']
-            setting = config.search([('company_id','=',company_id)])
+            company_id = sale.company_id and sale.company_id.id or 1
+            config = self.env['ir.config_parameter']
+            setting = config.search([('key','=','sale.default_deposit_product_id')])
             setting = setting and setting[0] or None
-            dep_product = setting and setting.deposit_default_product_id or None
+            dep_product = setting and setting.value or None
             if dep_product:
                 for line in sale.order_line:
-                    if line.product_id == dep_product:
-                        total_deps += line.price_total
-                sale.deposit_total = total_deps
+                    if str(line.product_id.id) == dep_product:
+                        total_deps += abs(line.price_unit)
+            sale.deposit_total = total_deps
                 
