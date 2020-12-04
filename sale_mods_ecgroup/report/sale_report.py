d2e0e9b062ee7aa63401db149aa7f0cbdb5d6701
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2020-12-04 15:07:39 -0800

    Updates to sales module 1. Added reseller_id to partners and put on sale report 2. Added approx_lead_time, sidemark, and customer code to sale form view and prints on report 3. Put in Less Deposits (-x) at subtotal place in sale report 4.. Modified sale report layout with css for top right info and middle info, etc

--- sale_mods_ecgroup/report/sale_report.py
+++ sale_mods_ecgroup/report/sale_report.py
@@ -7,6 +7,20 @@ from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    reseller_id = fields.Char(
+        'Reseller ID'
+        )
+    
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    comm_rate = fields.Float(
+        'Commissin Rate (%)', 
+        )
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
@@ -14,19 +28,38 @@ class SaleOrder(models.Model):
         'Total Deposits', 
         compute="_compute_deps_total",
         )
+    customer_code = fields.Char(
+        'Customer Code'
+        )
+    approx_lead_time = fields.Float(
+        'Approximate Lead Time'
+        )
+    sidemark = fields.Char(
+        'Sidemark'
+        )
+        
+    comm_total = fields.Float(
+        'Total Commisions', 
+        compute="_compute_deps_total",
+        )
     
     @api.depends('order_line')
     def _compute_deps_total(self):
         for sale in self:
             total_deps = 0
+            total_comm = 0
             company_id = sale.company_id and sale.company_id.id or 1
             config = self.env['ir.config_parameter']
             setting = config.search([('key','=','sale.default_deposit_product_id')])
             setting = setting and setting[0] or None
             dep_product = setting and setting.value or None
-            if dep_product:
-                for line in sale.order_line:
-                    if str(line.product_id.id) == dep_product:
-                        total_deps += abs(line.price_unit)
+            for line in sale.order_line:
+                if dep_product and str(line.product_id.id) == dep_product:
+                    total_deps += abs(line.price_unit)
+                if line.comm_rate:
+                    total_comm += line.comm_rate*line.price_unit*line.quanity/100
+            sale.comm_total = total_comm        
             sale.deposit_total = total_deps
-                
+            
+            
+            
