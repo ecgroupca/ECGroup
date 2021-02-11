c24616bc884a9b65d5d731ca5c1a4c585a8e9fd3
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2021-02-10 16:01:05 -0800

    change_dest_loc and stock_history modules committed to repo

---
+++ change_dest_location_rm-13.0.1.1/change_dest_location_rm/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Change Destination Location',
+    'summary': 'Change Destination Location For Purchase Orders',
+    'version': '1.1',
+    'category': 'Purchases',
+    'author': 'R&M',
+    "license": "LGPL-3",
+    "price": 30.00,
+    "currency": "EUR",
+    "images": ["images/thumbnail.jpg"],
+    'depends': ['purchase_stock'],
+    'init_xml': [],
+    'data': [ 
+        'view/views.xml',
+
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
---
+++ change_dest_location_rm-13.0.1.1/change_dest_location_rm/models/stock_picking.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models, _
+
+
+class Purchase(models.Model):
+    _inherit = "purchase.order"
+
+    def _get_destination_location(self):
+        self.ensure_one()
+        if not self.custom_dest_location_id:
+            return super(Purchase, self)._get_destination_location()
+        return self.custom_dest_location_id.id
+
+    custom_dest_location_id = fields.Many2one('stock.location', 'Custom Destination Location', check_company=True,
+                                              help="This is the custom destination location when you create a picking "
+                                                   "manually with this operation type. If it is empty, it will use the default location.")
