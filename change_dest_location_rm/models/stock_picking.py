a03054a643b8275c76a168da6ef0a39030150205
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2021-02-18 15:04:00 -0800

    [ADD] Added the same destination location id field to sales orders to replicate the way it works with PO's

--- change_dest_location_rm/models/stock_picking.py
+++ change_dest_location_rm/models/stock_picking.py
@@ -3,6 +3,19 @@
 from odoo import api, fields, models, _
 
 
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _get_destination_location(self):
+        self.ensure_one()
+        if not self.custom_dest_location_id:
+            return super(SaleOrder, self)._get_destination_location()
+        return self.custom_dest_location_id.id
+
+    custom_dest_location_id = fields.Many2one('stock.location', 'Custom Destination Location', check_company=True,
+                                              help="This is the custom destination location when you create a picking "
+                                                   "manually with this operation type. If it is empty, it will use the default location."
+
 class Purchase(models.Model):
     _inherit = "purchase.order"
 
