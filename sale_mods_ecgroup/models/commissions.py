11529c52baa5905aeb5a2a2da0891a760558b602
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2021-04-08 09:32:04 -0700

    [FIX] Fixes for access controls and for creating the invoice

--- sale_mods_ecgroup/models/commissions.py
+++ sale_mods_ecgroup/models/commissions.py
@@ -159,10 +159,11 @@ class SaleOrder(models.Model):
             # Manage the creation of invoices in sudo because a salesperson must be able to generate an invoice from a
             # sale order without "billing" access rights. However, he should not be able to create an invoice from scratch.
             moves = self.env['account.move'].sudo().with_context(default_type='in_invoice').create(invoice_vals_list)
-            order.has_comm_inv = moves and moves[0] and True or False
+            move_id = moves and moves[0] and moves[0].id or None           
+            order.has_comm_inv = move_id and True or False
             if moves and moves[0]:
                 for line in comm_lines:
-                    line.invoice_id = moves and moves[0] or False               
+                    line.invoice_id = moves and moves[0] and moves[0].id or False               
         return moves
     
 
