c84e68f803c5ddd49f8dec37c9cb6dd37b1263ed
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2021-07-15 14:57:08 -0700

    added computed field to get the max ship date for each sale

--- qb_transfer_locations/models/sale.py
+++ qb_transfer_locations/models/sale.py
@@ -11,3 +11,11 @@ class SaleOrder(models.Model):
         compute = '_get_shipped_date',
         )
         
+    @api.depends('picking_ids')
+    def _get_shipped_date(self):
+        for sale in self:
+            ship_dates = []
+            for pick in sale.picking_ids:
+                ship_dates.append(pick.date_done)  
+            sale.trans_shipped_date = max(ship_dates)                
+        
