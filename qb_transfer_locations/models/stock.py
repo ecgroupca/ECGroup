b67098338541cabbbafb3119a50e166d293d1835
DESKTOP-M13NBF3\thoma <thomaqui@gmail.com>
2021-08-27 11:12:41 -0700

    [Add] Bypass reservation fields and logic 1. Added booleans to denote products and stock moves that should have their reservations bypassed. 2. Rewrote/overwrote def _should_bypass_reservation() method on stock move to return true if either the stock move or product template bypass_reservation field is checked.

--- qb_transfer_locations/models/stock.py
+++ qb_transfer_locations/models/stock.py
@@ -14,11 +14,29 @@ class StockPicking(models.Model):
     location_dest_id = fields.Many2one(readonly=False)
     
     @api.onchange('location_id','location_dest_id')
-    def _onchange_sales_team(self):
+    def _onchange_locations(self):
         for picking in self:
             for move in picking.move_lines:
                 move.location_id = picking.location_id
                 move.location_dest_id = picking.location_dest_id
             for move_l in picking.move_line_ids:
                 move_l.location_id = picking.location_id
-                move_l.location_dest_id = picking.location_dest_id                    
+                move_l.location_dest_id = picking.location_dest_id   
+      
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    bypass_reservation = fields.Boolean('Bypass Reservation')                
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    bypass_reservation = fields.Boolean('Bypass Reservation')
+
+    def _should_bypass_reservation(self):
+        self.ensure_one()
+        bypass = self.location_id.should_bypass_reservation() 
+        bypass |= self.product_id.type != 'product'
+        bypass |= self.product_id.bypass_reservation
+        bypass |= self.bypass_reservation
+        return bypass
