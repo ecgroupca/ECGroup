85bcf5017a993d78aa4729bc38e7e0e6e6c5b44f
DESKTOP-E2KASR6\thoma <aoconnor@quickbeamllc.com>
2021-09-28 13:25:57 -0700

    [FIX-MOD] Fixed trans_shipped_date field to be store=True so we could use its value in domain for report Put in all required fields for report Make qb_transfer_locations a dependency of module so that trans_shipped_date would be accessible Modified domain for sales on report to not default to today's date if no date from and/or date to entered

--- qb_unbilled_closed_sales/reports/sales_report.py
+++ qb_unbilled_closed_sales/reports/sales_report.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 class UnbilledSalesReport(models.AbstractModel):
 
-    _name = 'report.qb_unbilled_closed_sales.report_open_sales'
+    _name = 'report.qb_unbilled_closed_sales.report_closed_unbilled_sales'
     _description = 'Unbilled Shipped Sales Report'
     
     @api.model
@@ -16,11 +16,11 @@ class UnbilledSalesReport(models.AbstractModel):
         date_from = date_to = fields.Date.today()
         sale_obj = self.env['sale.order']        
         if not docids:
-            date_from = fields.Date.from_string(data['form'].get('date_from')) or fields.Date.today()
-            date_to = fields.Date.from_string(data['form'].get('date_to')) or fields.Date.today()
+            date_from = fields.Date.from_string(data['form'].get('date_from'))
+            date_to = fields.Date.from_string(data['form'].get('date_to'))
             showroom = data['form'].get('showroom', False)
             company_id = data['form'].get('company_id', False)
-            company_id = company_id and company_id[0] or None
+            company_id = company_id and company_id[0] or None        
             domain = [('company_id','=',company_id),('state','not in',['draft','cancel','sent'])]
             domain += [('invoice_status','=','to invoice'),('trans_shipped_date','!=',False)]
             if date_from and date_to:
