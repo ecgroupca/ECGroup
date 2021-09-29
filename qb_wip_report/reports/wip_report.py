4320c0f28d242e318312f4e2248d73cd3d5b0bb5
DESKTOP-E2KASR6\thoma <aoconnor@quickbeamllc.com>
2021-09-28 17:59:29 -0700

    [MOD] WIP Report change to bring in workorders in progress only

--- qb_wip_report/reports/wip_report.py
+++ qb_wip_report/reports/wip_report.py
@@ -18,7 +18,7 @@ class ReportWIPReport(models.AbstractModel):
             
             workcenter_ids = data['form'].get('workcenter_id', False)
             user_id = data['form'].get('user_id', False)
-            domain = [('production_id.state','not in',['draft','cancel','done','confirmed'])]              
+            domain = [('state','=','progress'),('production_id.state','not in',['draft','cancel','done','confirmed'])]              
             if workcenter_ids:
                 domain.append(('workcenter_id','in',workcenter_ids))
             if user_id:
