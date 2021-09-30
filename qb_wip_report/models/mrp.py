5678637253fe31d411bb72571a2853abca1d34e7
DESKTOP-E2KASR6\thoma <aoconnor@quickbeamllc.com>
2021-09-30 08:39:51 -0700

    [REMOVE] remove bypass_res and report changes 1. remove all bypass_reservations func 2. Add next workorder computed field 3. Unbilled closed sales report changes

--- qb_wip_report/models/mrp.py
+++ qb_wip_report/models/mrp.py
@@ -1,6 +1,22 @@
 from odoo import api, fields, models
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class MRPWorkorder(models.Model):
+    _inherit = "mrp.workorder"
     
-    sidemark = fields.Char("Sidemark")
+    next_wo_id = fields.Many2one(
+        'Next Workorder',
+        compute = '_get_next_wo',
+        )
+        
+    @api.depends('production_id')
+    def _get_next_wo(self):
+        for wo in self:
+            #prod_id = wo.production_id
+            #routing_id = prod_id.routing_id
+            wo.next_wo_id = False
+            for next_wo in wo.next_work_order_ids:
+                wo.next_wo_id = next_wo
+                break
+            if not wo.next_wo_id:
+                #then we have to lookup the 
+            
