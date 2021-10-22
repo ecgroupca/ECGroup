e41b8b570bfe5d26afe99fb60eea2ca2c35345f1
Adam O'Connor <thomaqui@gmail.com>
2021-10-21 18:45:59 -0700

    [MOD] Sales commissions remove paid comm invoice sales orders

--- qb_commissions_ecgroup/wizard/sale_commission_report.py
+++ qb_commissions_ecgroup/wizard/sale_commission_report.py
@@ -13,28 +13,28 @@ class ReportSaleCommissionReport(models.AbstractModel):
         date_from = fields.Date.from_string(data['form'].get('date_from')) or fields.Date.today()
         date_to = fields.Date.from_string(data['form'].get('date_to')) or fields.Date.today()
         showroom = data['form'].get('showroom', False)
-        remove_paid = data['form'].get('remove_paid', False)       
-        domain_search = [('invoice_status','!=','to invoice'),('inv_bal_due','=',0),('open_shipments','!=',True),('comm_total','>',0),('create_date','>=',date_from),('create_date','<=',date_to)]
+        remove_paid = data['form'].get('remove_paid', False)   
+        #create the domain for sales eligible for commissions       
+        domain_search = [('invoice_status','!=','to invoice'),('inv_bal_due','=',0),('open_shipment','=',False),('comm_total','>',0),('create_date','>=',date_from),('create_date','<=',date_to)]
         if showroom:
             domain_search.append(('team_id','in',showroom))
-        if remove_paid:
-            domain_search.append(('comm_inv_id.amount_residual','=',0),('comm_inv_id.amount_total','>',0))
-        comm_sales = self.env['sale.order'].search(domain_search)     
+        comm_sales = self.env['sale.order'].search(domain_search)  
+        if remove_paid and comm_sales:
+            domain_search =['|',('comm_inv_id','=',False),('comm_inv_id','!=',False),('comm_inv_id.amount_residual','>',0),('comm_inv_id.amount_total','>',0)]
+            comm_sales.search(domain_search)     
         sale_comm = {}
         for commission in comm_sales:
             customer_key = 'c_%s'%(commission.partner_id.id)
             team_name = 'No_Name'
             if commission.team_id:
-                team_name = commission.team_id.name.replace(" ","_")
-            
+                team_name = commission.team_id.name.replace(" ","_")           
             if team_name in sale_comm:
                 if customer_key in sale_comm[team_name]:
                     sale_comm[team_name][customer_key]['data'].append(commission)
                 else:
                     sale_comm[team_name].update({customer_key:{'name':commission.partner_id.name,'ref':commission.partner_id.ref ,'id':commission.partner_id.id,'data':[commission]}})
             else:
-                sale_comm.update({team_name:{customer_key:{'name':commission.partner_id.name,'ref':commission.partner_id.ref,'id':commission.partner_id.id,'data':[commission]}}})
-        
+                sale_comm.update({team_name:{customer_key:{'name':commission.partner_id.name,'ref':commission.partner_id.ref,'id':commission.partner_id.id,'data':[commission]}}})     
         return {
             'doc_ids': comm_sales.ids,
             'doc_model': 'sale.order',
