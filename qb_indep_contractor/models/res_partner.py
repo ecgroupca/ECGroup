6fc3afad9b3f6b724881cf75e42ab632edca2a03
Adam O'Connor <aoconnor@quickbeamllc.com>
2021-10-28 09:08:23 -0700

    [MODS]  Set up the final report for 1099 contractors 1. fixed report to display one line per vendor on qweb report 2. fixed logic to get docids from active selection (list view) or one-off from the partner form and display them on report if "Needs 1099" box is checked. 3. fixed logic in calculating billing_address_id and total_paid_current_year in computed fields function.  Will only assign default values of 0 and false if needs 1099 is unchecked.

--- qb_indep_contractor/models/res_partner.py
+++ qb_indep_contractor/models/res_partner.py
@@ -16,25 +16,27 @@ class ResPartner(models.Model):
     @api.depends('needs_ten_ninety_nine')
     def _get_billing_total_paid(self):
         for indep in self:
-            domain = []
             indep.total_paid_current_year = 0
-            now = datetime.datetime.now()
-            date = now.date()
-            year = date.strftime("%Y")
-            bill_address = indep.address_get(['invoice'])['invoice']
-            indep.billing_address_id = bill_address
-            if bill_address:
-                #search for all paid vendor bills for this billing address_get
-                domain = [('type','=','in_invoice')]
-                domain.append(('date','>=',year + '-01-01 00:00:00'))
-                domain.append(('date','<=',year + '-12-31 23:59:59'))
-                domain.append(('partner_id','=',bill_address))
-                bills = self.env['account.move'].search(domain)
-                total_paid = 0
-                for bill in bills:
-                    if bill.state == 'posted':
-                        total_paid += bill.amount_total - bill.amount_residual
-                indep.total_paid_current_year = total_paid
+            indep.billing_address_id = False
+            if indep.needs_ten_ninety_nine:
+                domain = []               
+                now = datetime.datetime.now()
+                date = now.date()
+                year = date.strftime("%Y")
+                bill_address = indep.address_get(['invoice'])['invoice']
+                indep.billing_address_id = bill_address
+                if bill_address:
+                    #search for all paid vendor bills for this billing address_get
+                    domain = [('type','=','in_invoice')]
+                    domain.append(('date','>=',year + '-01-01 00:00:00'))
+                    domain.append(('date','<=',year + '-12-31 23:59:59'))
+                    domain.append(('partner_id','=',bill_address))
+                    bills = self.env['account.move'].search(domain)
+                    total_paid = 0
+                    for bill in bills:
+                        if bill.state == 'posted':
+                            total_paid += bill.amount_total - bill.amount_residual
+                    indep.total_paid_current_year = total_paid
                            
                 
             
