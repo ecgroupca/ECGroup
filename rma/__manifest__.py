cd6cdcc131853367d24e49adaa3b6f8ef47ae37f
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-02-11 12:59:12 -0800

    [FIX] Added dependency for stock picking sale order link to bring in sale_id field

--- rma/__manifest__.py
+++ rma/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["ernestotejeda"],
     "license": "AGPL-3",
-    "depends": ["account","stock","qb_indep_contractor"],
+    "depends": ["account","stock","qb_indep_contractor","stock_picking_sale_order_link"],
     "data": [
         "views/report_rma.xml",
         "report/report.xml",
