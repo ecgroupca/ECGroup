d3b67fbb3476f4bd3c95c19c3de4f50cfead6dfe
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-02-25 12:11:08 -0800

    [FIX] Fixed Excel version of open sales report for open shipments flag consideration

--- qb_opensales_reports/reports/sales_report.py
+++ qb_opensales_reports/reports/sales_report.py
@@ -31,7 +31,7 @@ class OpenSalesXlsx(models.AbstractModel):
         #sales_from_to = sale_obj.search(date_domain)
         #compute open shipments/production for the orders in docids 
         #sales_from_to._compute_open_shipments()               
-        domain_search = [('open_shipment','!=',False)]
+        domain_search = [('open_shipment','=',True)]
         domain_search += date_domain
         if showroom:
             domain_search.append(('team_id','in',showroom))
@@ -110,7 +110,7 @@ class OpenSalesReport(models.AbstractModel):
                 #sales_from_to = sale_obj.search(date_domain)
                 #compute open shipments/production for the orders in docids 
                 #sales_from_to._compute_open_shipments()               
-                domain_search = [('open_shipment','!=',False)]
+                domain_search = [('open_shipment','=',True)]
                 domain_search += date_domain
                 if showroom:
                     domain_search.append(('team_id','in',showroom))
