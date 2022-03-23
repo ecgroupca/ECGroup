1ae1134708128237f21444be3cfb9e3e7ddb991d
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-03-23 06:16:54 -0700

    [MODS] Modifications to the reserved orders fields for quants and variants      Tested and found that reserved order lines were missing manufacturing orders and internal transfers.   Found the relationship between move lines to stock moves to raw material production orders and   revised the code to include the manufacturing orders in the reserved order reports in the "update qty"   tab and on the Item Details Report

--- qb_itemdetails_reports/models/product.py
+++ qb_itemdetails_reports/models/product.py
@@ -16,19 +16,21 @@ class ProductProduct(models.Model):
             res_line_ids = prod.reserved_line_ids
             orders = {}
             for line in res_line_ids:           
-                trans = line.picking_id or line.production_id or None
-                if trans and trans.name in orders:
-                    orders[trans.id] = trans.name 
-                else:
-                    vals = {
-                        'product_id': prod.id,
-                        'move_line_id': line.id,
-                        
-                        'product_uom_qty': line.product_uom_qty,                        
-                        'name': trans and trans.name or 'No order',                            
-                    }
-                    reserved_order = self.env['reserved.order'].sudo().create(vals)
-                    prod.reserved_order_ids = [(4, reserved_order.id)]  
+                trans = line.move_id.raw_material_production_id or line.picking_id or None
+                pick_type = trans and trans.picking_type_id or None
+                if pick_type and pick_type.code in ['mrp_operation','outgoing','internal']:
+                    if line.state not in ['cancel','done']:
+                        if trans and trans.name in orders:
+                            orders[trans.id] = trans.name 
+                        else:
+                            vals = {
+                                'product_id': prod.id,
+                                'move_line_id': line.id,                      
+                                'product_uom_qty': line.product_uom_qty,                        
+                                'name': trans and trans.name or 'No order',                            
+                            }
+                            reserved_order = self.env['reserved.order'].sudo().create(vals)
+                            prod.reserved_order_ids = [(4, reserved_order.id)]  
     
     
                         
