fd9ee7a9eb9fd6ea1e20bb188de9c8289b33f674
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-05-20 08:46:01 -0700

    [FIX] Committed the import for access rights for wizards

--- qb_purchase_approvals/models/approval_request.py
+++ qb_purchase_approvals/models/approval_request.py
@@ -18,15 +18,6 @@ class ApprovalRequest(models.Model):
         ('refused', 'Refused'),
         ('cancel', 'Cancel')],
         )
-
-    user_status = fields.Selection([
-        ('new', 'New'),
-        ('pending', 'To Approve'),
-        ('approved', 'Approved'),
-        ('rfqs', 'RFQs Created'),
-        ('refused', 'Refused'),
-        ('cancel', 'Cancel')],
-        )
         
     def _compute_purchase_orders(self):
         for approval in self:
@@ -39,45 +30,8 @@ class ApprovalRequest(models.Model):
                 if purch_id not in purchase_orders:
                     purchase_orders.append(purch_id)
                     approval.purchase_order_ids = [(4, purch_id)]
-            if purchase_orders and approval.request_status == 'approved':
-                approval.request_status = 'rfqs'  
-                for approver in approval.approver_ids:
-                    if approver.status == 'approved':
-                        approver.status = 'rfqs'                       
-                    
-    @api.depends('approver_ids.status')
-    def _compute_request_status(self):
-        for request in self:
-            status_lst = request.mapped('approver_ids.status')
-            minimal_approver = request.approval_minimum if len(status_lst) >= request.approval_minimum else len(status_lst)
-            if status_lst:
-                if status_lst.count('cancel'):
-                    status = 'cancel'
-                elif status_lst.count('refused'):
-                    status = 'refused'
-                elif status_lst.count('new'):
-                    status = 'new'
-                elif status_lst.count('approved') >= minimal_approver:
-                    status = 'approved'
-                elif status_lst.count('rfqs'):
-                    status = 'rfqs'
-                else:
-                    status = 'pending'
-            else:
-                status = 'new'
-            request.request_status = status
-            
-class ApprovalApprover(models.Model):
-    _inherit = 'approval.approver'
-
-    status = fields.Selection([
-        ('new', 'New'),
-        ('pending', 'To Approve'),
-        ('approved', 'Approved'),
-        ('rfqs', 'RFQs Created'),
-        ('refused', 'Refused'),
-        ('cancel', 'Cancel')]
-    )
+            if purchase_orders and approval.request_status == 'approved': 
+                approval.request_status = 'rfqs'                          
     
     
                         
