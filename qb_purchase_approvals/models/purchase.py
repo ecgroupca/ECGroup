cf9f7be7c604a2c931af8037a38ba4a34a981fc7
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-05-23 16:40:51 -0700

    fix for field def - incorrect model

--- qb_purchase_approvals/models/purchase.py
+++ qb_purchase_approvals/models/purchase.py
@@ -4,12 +4,7 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
     
-    """acknowledged = fields.Boolean('Acknowledged',track_visibility='onchange')
-    shipped = fields.Boolean('Shipped',track_visibility='onchange')
-    received = fields.Boolean('Received',track_visibility='onchange')
-    late order = fields.Boolean('Late Order',track_visibility='onchange')"""
-    
     approval_id = fields.Many2one(
-        'purchase.approval',
+        'approval.request',
         'Approval', 
         )
