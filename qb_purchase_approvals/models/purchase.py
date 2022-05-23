458d70acd89a5c5dd02d483598c0adca4e965b35
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-05-23 16:08:54 -0700

    fixes syntax error

--- qb_purchase_approvals/models/purchase.py
+++ qb_purchase_approvals/models/purchase.py
@@ -4,10 +4,11 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
     
-    acknowledged = fields.Boolean('Acknowledged',track_visibility='onchange')
+    """acknowledged = fields.Boolean('Acknowledged',track_visibility='onchange')
     shipped = fields.Boolean('Shipped',track_visibility='onchange')
     received = fields.Boolean('Received',track_visibility='onchange')
-    late order = fields.Boolean('Late Order',track_visibility='onchange')
+    late order = fields.Boolean('Late Order',track_visibility='onchange')"""
+    
     approval_id = fields.Many2one(
         'purchase.approval',
         'Approval', 
