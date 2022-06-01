0f679680d03b87ad85d4635bf235bab128cfb664
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-01 14:59:06 -0700

    [FIX] fixes compute_sale_orders method for getting the sales associated with purchases

--- sale_mrp_link/models/purchase_order.py
+++ sale_mrp_link/models/purchase_order.py
@@ -1,70 +1,47 @@
 # Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
 
-    production_ids = fields.One2many('mrp.production', 'sale_order_id')
-    production_count = fields.Integer(
-        compute='_compute_production_count', store=True)
-
-    purchase_ids = fields.Many2many(
-        'purchase.order',
-        string = 'Purchase Orders',
-        compute="_compute_purchase_orders",
+    sale_order_id = fields.Many2many(
+        'sale.order',
+        string = 'Sale Orders',
+        compute = "_compute_sale_orders",
+        readonly = False,
+        store = True,
     )
-    
-    @api.depends("production_ids")
-    def _compute_production_count(self):
-        for sale in self:
-            sale.production_count = len(sale.production_ids)
 
-    def action_view_production(self):
-        action = self.env.ref('mrp.mrp_production_action').read()[0]
-        if self.production_count > 1:
-            action['domain'] = [('id', 'in', self.production_ids.ids)]
-        else:
-            action['views'] = [
-                (self.env.ref('mrp.mrp_production_form_view').id, 'form')]
-            action['res_id'] = self.production_ids.id
-        return action
+    sale_order_count = fields.Integer(
+        "Number of Source Sale",
+        compute='_compute_sale_order_count',
+        groups='sales_team.group_sale_salesman')
         
-    @api.depends("production_ids")
-    def _compute_purchase_count(self):
-        for sale in self:
-            sale.production_count = len(sale.production_ids)
+    @api.depends('order_line.sale_order_id','sale_order_id')
+    def _compute_sale_order_count(self):
+        for purchase in self:
+            count = len(purchase.sale_order_id)
+            purchase.sale_order_count = count
 
-    def _compute_purchase_orders(self):
-        purch_obj = self.env['purchase.order']
-        for sale in self:
-            sale.purchase_order_ids = [(4, False)]
+    @api.depends('order_line.sale_order_id','sale_order_id')    
+    def _compute_sale_orders(self):
+        for purchase in self:
+            purchase.sale_order_id = [(4, False)]
             #search for purchases that reference the sale
-            domain = [('sale_order_id','in',sale.id)]
-            domain += [('company_id','=',line.company_id.id)]
-            purchase_ids = purchase_obj.search(domain)
-            purch_ids = purchase_ids.ids
-            sale.purchase_order_ids = [(6, 0, purch_ids)]
+            domain = [('id','in',purchase.order_line.sale_order_id.ids)]
+            domain += [('id','in',purchase.sale_order_id.ids)]
+            domain += [('company_id','=',purchase.company_id.id)]
+            sale_ids = self.env['sale.order'].search(domain)
+            purchase.sale_order_id = [(6, 0, sale_ids.ids)]
             
-    def action_view_purchase(self):
+    def action_view_sale_orders(self):
         self.ensure_one()
         # Force active_id to avoid issues when coming from smart buttons
         # in other models
-        action = (
-            self.env.ref("purchase.purchase_form_action")
-            .with_context(active_id=self.id)
-            .read()[0]
-        )
-        purchases = self.purchase_ids
-        if len(purchases) > 1:
-            action["domain"] = [("id", "in", purchases.ids)]
-        elif purchases:
-            action.update(
-                res_id=purchases.id, 
-                view_mode="form", 
-                view_id=False, 
-                views=False,
-            )
-        return action
+        sale_order_ids = self.sale_order_id.ids
+        action = {
+            'res_model': 'sale.order',
+            'type': 'ir.actions.act_window',
