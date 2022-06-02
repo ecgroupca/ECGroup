0d9cd2adb82df5e0a58b0fac6f01fe28e1fb5018
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-02 14:13:36 -0700

    added sale_order.xml to add button for purchase deposit

--- purchase_deposit/__manifest__.py
+++ purchase_deposit/__manifest__.py
@@ -15,6 +15,7 @@
         "wizard/purchase_make_invoice_advance_views.xml",
         "views/res_config_settings_views.xml",
         "views/purchase_view.xml",
+        "views/sale_order.xml",
         "report/external_layout_ec_logo.xml",
         "report/report_purchase_ec_logo.xml",
         "report/report_purchase_ec_logo_document.xml",
