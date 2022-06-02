cb00fb58343b9fcf4915a5f35f96ec41fa729fdc
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-02 12:05:42 -0700

    [MODS] Links from sale order to POs, POs to MOs, etc 1. Put sale order field from PO in the search so they can search by it and not just by ref field.     - Add SO link on PO in header badge.[DONE] 2. Put link to POs on sales order[DONE] 3. Put link to sale as badge on MO and allow for search on sale_order_id [DONE] 4. Put back notes and etwo sales reference on MOs [DONE] 5. Put in changes on sale external barcode labels to grab the description from the sale order line instead of product's description

--- sale_mrp_link/models/sale_order.py
+++ sale_mrp_link/models/sale_order.py
@@ -11,6 +11,15 @@ class SaleOrder(models.Model):
     production_count = fields.Integer(
         compute='_compute_production_count', store=True)
 
+    purchase_order_ids = fields.Many2many(
+        'purchase.order',
+        string = 'Purchase Orders',
+        compute="_compute_purchase_orders",
+    )
+    
+    purchase_count = fields.Integer(
+        compute='_compute_purchase_count', store=True)
+    
     @api.depends("production_ids")
     def _compute_production_count(self):
         for sale in self:
@@ -24,4 +33,41 @@ class SaleOrder(models.Model):
             action['views'] = [
                 (self.env.ref('mrp.mrp_production_form_view').id, 'form')]
             action['res_id'] = self.production_ids.id
+        return action
+        
+    @api.depends("purchase_order_ids")
+    def _compute_purchase_count(self):
+        for sale in self:
+            sale.purchase_count = len(sale.purchase_order_ids)
+
+    def _compute_purchase_orders(self):
+        purch_obj = self.env['purchase.order']
+        for sale in self:
+            sale.purchase_order_ids = [(4, False)]
+            #search for purchases that reference the sale
+            domain = [('sale_order_id','in',sale.id)]
+            domain += [('company_id','=',sale.company_id.id)]
+            purchase_ids = purch_obj.search(domain)
+            purch_ids = purchase_ids.ids
+            sale.purchase_order_ids = [(6, 0, purch_ids)]
+            
+    def action_view_purchases(self):
+        self.ensure_one()
+        # Force active_id to avoid issues when coming from smart buttons
+        # in other models
+        action = (
+            self.env.ref("purchase.purchase_form_action")
+            .with_context(active_id=self.id)
+            .read()[0]
+        )
+        purchases = self.purchase_ids
+        if len(purchases) > 1:
+            action["domain"] = [("id", "in", purchases.ids)]
+        elif purchases:
+            action.update(
+                res_id=purchases.id, 
+                view_mode="form", 
+                view_id=False, 
+                views=False,
+            )
         return action
