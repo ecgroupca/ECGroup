4f313c4b6e8d40a2e8162581e9c3faf6a574a22e
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-02 14:52:34 -0700

    adds the necessary import '_' to the purchase.py file for use in def action_view_sales() method.

--- purchase_deposit/models/purchase.py
+++ purchase_deposit/models/purchase.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import fields, models, api, _
 
 
 class PurchaseOrder(models.Model):
