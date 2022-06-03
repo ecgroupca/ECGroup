8d369e378e28b7dc46eb1e6f52b0b634954c84cd
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-03 15:34:28 -0700

    testing limiting wo delete access

---
+++ qb_mrp_workorders/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'QB Product Links',
+    'description': """Modifies record rules for basic MO users 
+        so that they can only create and view and not update nor delete them.""",
+    'sequence': 1,
+    'version': '15,1.0.0',
+    'author': 'Adam OConnor, Quickbeam LLC <aoconnor@quickbeamllc.com>',
+    'website': 'https://quickbeamllc.com',
+    'depends': [
+        'mrp',
+        ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'installable': True,
+}
