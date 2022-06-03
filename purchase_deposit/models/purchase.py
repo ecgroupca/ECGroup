f5b98c950ce1ef0b7e57292985efe6644b59103f
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-03 10:36:44 -0700

    test

--- purchase_deposit/models/purchase.py
+++ purchase_deposit/models/purchase.py
@@ -1,7 +1,3 @@
-# Copyright 2019 Elico Corp, Dominique K. <dominique.k@elico-corp.com.sg>
-# Copyright 2019 Ecosoft Co., Ltd., Kitti U. <kittiu@ecosoft.co.th>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo import fields, models, api, _
 
 
