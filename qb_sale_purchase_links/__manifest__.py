90ea0ee092e5bf67ef3329785fe4129646c244c9
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-07 07:59:17 -0700

    fixed depends and updated version#

--- qb_sale_purchase_links/__manifest__.py
+++ qb_sale_purchase_links/__manifest__.py
@@ -2,12 +2,12 @@
     'name': 'QB Sale Purchase Linkds',
     'description': 'Provides links from purchase to sales and vice versa.',
     'sequence': 1,
-    'version': '1.0.0',
+    'version': '13.1.0.0',
     'author': 'Adam O\'Connor <aoconnor@quickbeamllc.com>',
     'website': 'https://quickbeamllc.com',
-    'depends': ['stock','product'],
+    'depends': ['sale_purchase'],
     'data': [
-        'views/purchase_views.xml',
+        'views/purchase_order.xml',
         'views/sale_order.xml'
     ],
     'qweb': [],
