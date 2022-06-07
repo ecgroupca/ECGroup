64ebf77781ca4e9f66b7aa724b6df8f6651208e0
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-07 16:58:59 -0700

    fixing syntax - missing commas

--- qb_purchase_approvals/models/approval_request.py
+++ qb_purchase_approvals/models/approval_request.py
@@ -30,7 +30,7 @@ class ApprovalRequest(models.Model):
         return [("id", "child_of", buyer_users.ids)]
     
     user_id = fields.Many2one('res.users',
-        string = 'Purchase Representative'
+        string = 'Purchase Representative',
         domain = _compute_rep_domain,
         )
                     
