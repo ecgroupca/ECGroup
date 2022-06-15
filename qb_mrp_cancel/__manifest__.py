9ecb28e304fa278b58f9e442cef67d7bf7735cb6
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-06-15 07:40:52 -0700

    [UPD] Asks for confirmation

--- qb_mrp_cancel/__manifest__.py
+++ qb_mrp_cancel/__manifest__.py
@@ -1,15 +1,15 @@
 {
     'name': 'QB MRP Cancel',
-    'description': """Cancel an MO and it cancels all of the pickings.""",
+    'description': """Cancel an MO and it cancels all of the pickings and provides a return for each of the done pickings.""",
     'sequence': 1,
-    'version': '1.0.0',
+    'version': '15.1.0.1',
     'author': 'Adam OConnor, Quickbeam LLC <aoconnor@quickbeamllc.com>',
     'website': 'https://quickbeamllc.com',
     'depends': [
         'mrp',
         ],
     'data': [
-        'wizard/mrp_returns_wizard_view.xml',
+        'views/mrp_views.xml',
     ],
     'application': True,
     'installable': True,
