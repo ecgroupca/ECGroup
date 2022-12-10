537e519e3bff39d551b0e331da65cde8e88d193b
Adam O'Connor <aoconnor@quickbeamllc.com>
2022-12-10 09:49:21 -0800

    [INITIAL] Initial commit of QB MRP Barcoding App 1.  Overrode the stock_barcode kanban view to include operation type, 'Manufacturing' 2. Added new client action for MRP workorders

---
+++ qb_mrp_barcoding/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'MRP Barcoding',
+    'description': 'Extends Barcoding to Manufacturing and Workorders',
+    'sequence': 1,
+    'version': '1.0.0',
+    'author': 'Adam OConnor <aoconnor@quickbeamllc.com>',
+    'website': 'https://quickbeamllc.com',
+    'category': 'Operations/Inventory',
+    'depends': [
+        'mrp',
+        'stock_barcode',
+    ],
+    'data': [
+        'views/mrp_barcoding_views.xml',   
+    ],
+    'application': True,
+    'installable': True,
+}
