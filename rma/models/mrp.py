e3d08d3fed8ad5a1a119c53b78f8edb584922efd
Adam O'Connor <aoconnor@quickbeamllc.com>
2023-03-09 16:11:57 -0800

    [FIX] Removes requirement that the product consumed must be different than the product produced.

--- rma/models/mrp.py
+++ rma/models/mrp.py
@@ -22,8 +22,21 @@ class MRPProduction(models.Model):
         return res
 
     def button_mark_done(self):
-        res = super().button_mark_done()
-        if res:
-            self.rma_id.state = 'repaired'
-        return res
+        self.ensure_one()
+        self._check_company()
+        for wo in self.workorder_ids:
+            if wo.time_ids.filtered(lambda x: (not x.date_end) and (x.loss_type in ('productive', 'performance'))):
+                raise UserError(_('Work order %s is still running') % wo.name)
+        self._check_lots()
+
+        self.post_inventory()
+        # Moves without quantity done are not posted => set them as done instead of canceling. In
+        # case the user edits the MO later on and sets some consumed quantity on those, we do not
+        # want the move lines to be canceled.
+        (self.move_raw_ids | self.move_finished_ids).filtered(lambda x: x.state not in ('done', 'cancel')).write({
+            'state': 'done',
+            'product_uom_qty': 0.0,
+        })
+        self.rma_id.state = 'repaired'
+        return self.write({'date_finished': fields.Datetime.now()})
 
