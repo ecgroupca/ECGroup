f5ec34b05d7d12fc26b74af572d0fe13e2eab04d
Adam O'Connor <aoconnor@quickbeamllc.com>
2023-05-02 07:03:19 -0700

    [MOD] Include open steps for barcoding boolean

--- pbj_mrp_routing_parallel/models/mrp_routing.py
+++ pbj_mrp_routing_parallel/models/mrp_routing.py
@@ -5,3 +5,4 @@ class MrpRouting(models.Model):
     _inherit = 'mrp.routing'
 
     parallel_steps = fields.Boolean(string="Parallel Steps")
+    barcoding_steps = fields.Boolean(string="Barcoding - All Open Steps")
