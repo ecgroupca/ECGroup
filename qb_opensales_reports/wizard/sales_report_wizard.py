953bcb9feedebc86fd2be949752c43159cdf4337
Adam O'Connor <aoconnor@quickbeamllc.com>
2024-01-11 13:34:03 -0800

    removes unnecessary fields for completed sales report

--- qb_opensales_reports/wizard/sales_report_wizard.py
+++ qb_opensales_reports/wizard/sales_report_wizard.py
@@ -8,8 +8,6 @@ class CompSalesReportWizard(models.TransientModel):
     company_id = fields.Many2one("res.company",string="Company",required=True)
     date_from = fields.Date("Date From", required=False)
     date_to = fields.Date("Date To", required=False)
-    showroom = fields.Many2many("crm.team",'sales_crm_rel_transient', 'sales_report_id', 'crm_team_id', string="Showroom")
-    sale_ids = fields.Many2many("sale.order",'sales_report_rel_transient', 'sales_report_id', 'sale_order_id', string="Sales")
     print_excel = fields.Boolean("Print in Excel")
     sales_rep_id = fields.Many2one("res.partner",string="Sales Rep.") 
     
@@ -18,7 +16,7 @@ class CompSalesReportWizard(models.TransientModel):
         data = {}
         data['ids'] = self.env.context.get('active_ids', [])
         data['model'] = self.env.context.get('active_model', 'ir.ui.menu')
-        data['form'] = self.read(['sales_rep_id','company_id','date_from', 'date_to', 'showroom','sale_ids'])[0]
+        data['form'] = self.read(['sales_rep_id','company_id','date_from', 'date_to'])[0]
         #used_context = self._build_contexts(data)
         #data['form']['used_context'] = dict(used_context, lang=get_lang(self.env).code)
         #return self.with_context(discard_logo_check=True)._print_report(data)
