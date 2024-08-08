850ed5566cdd0b28f35fd8fcae77b878442a50c6
Adam O'Connor <aoconnor@quickbeamllc.com>
2024-08-08 16:38:57 -0700

    removed all views from ICT app

--- intercompany_transaction_ept/__manifest__.py
+++ intercompany_transaction_ept/__manifest__.py
@@ -17,23 +17,23 @@
     # Dependencies
     'depends': ['delivery', 'purchase_stock', 'barcodes'],
     'data': [
-        'data/ir_sequence.xml',
-        'data/inter_company_transfer_config.xml',
+        #'data/ir_sequence.xml',
+        #'data/inter_company_transfer_config.xml',
 
-        'views/inter_company_transfer_ept.xml',
-        'views/inter_company_transfer_config_ept.xml',
-        'views/res_company.xml',
-        'views/sale.xml',
-        'views/purchase.xml',
-        'views/stock_picking.xml',
-        'views/account_move.xml',
-        'views/inter_company_transfer_log_book_ept.xml',
+        #'views/inter_company_transfer_ept.xml',
+        #'views/inter_company_transfer_config_ept.xml',
+        #'views/res_company.xml',
+        #'views/sale.xml',
+        #'views/purchase.xml',
+        #'views/stock_picking.xml',
+        #'views/account_move.xml',
+        #'views/inter_company_transfer_log_book_ept.xml',
 
-        'security/inter_company_transfer_security.xml',
-        'security/ir.model.access.csv',
+        #'security/inter_company_transfer_security.xml',
+        #'security/ir.model.access.csv',
 
-        'wizards/reverse_inter_company_transfer_ept.xml',
-        'wizards/import_export_products_ept.xml',
+        #'wizards/reverse_inter_company_transfer_ept.xml',
+        #'wizards/import_export_products_ept.xml',
 
         ],
 
