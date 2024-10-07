3906982d62d7f4e9d2cd7acff8d0c68a4b7fd3dc
Adam O'Connor <aoconnor@quickbeamllc.com>
2024-10-07 13:40:37 -0700

    added in purchase order line and updated the purchase order python code.

--- intercompany_transaction_ept/models/__init__.py
+++ intercompany_transaction_ept/models/__init__.py
@@ -7,6 +7,7 @@ from . import inter_company_transfer_config_ept
 from . import inter_company_transfer_log_ept
 from . import account_move
 from . import purchase
+from . import purchase_order_line
 from . import sale
 from . import res_company
 from . import stock_picking
