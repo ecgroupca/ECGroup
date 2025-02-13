13d166f40a0cad24eb7cb1e8498de8ff806c05df
Adam O'Connor <aoconnor@quickbeamllc.com>
2025-02-13 13:22:27 -0800

    refactoring the code with all spaces for indentation and no tabs

--- bi_dynamic_barcode_labels/report/report_barcode_product_labels.py
+++ bi_dynamic_barcode_labels/report/report_barcode_product_labels.py
@@ -5,32 +5,35 @@ from odoo import api, fields, models
 
 
 class DynamicBarcodeProductLabelsParser(models.AbstractModel):
-	_name = 'report.bi_dynamic_barcode_labels.product_barcode_labels'
-	_description = "Product variant barcode labels Report"
+    _name = 'report.bi_dynamic_barcode_labels.product_barcode_labels'
+    _description = "Product variant barcode labels Report"
 
-	def _get_barcode_details_info(self):
-		barcode_config = \
-			self.env.ref('bi_dynamic_barcode_labels.barcode_labels_config_data')
-		return {
-			'barcode_type': barcode_config.barcode_type,
-			'barcode_width': barcode_config.barcode_width,
-			'barcode_height': barcode_config.barcode_height,
-			'barcode_currency_id': barcode_config.barcode_currency_id,
-			'barcode_currency_position': barcode_config.barcode_currency_position,
-		}
+    def _get_barcode_details_info(self):
+        barcode_config = \
+        self.env.ref('bi_dynamic_barcode_labels.barcode_labels_config_data')
+        
+        return {
+            'barcode_type': barcode_config.barcode_type,
+            'barcode_width': barcode_config.barcode_width,
+            'barcode_height': barcode_config.barcode_height,
+            'barcode_currency_id': barcode_config.barcode_currency_id,
+            'barcode_currency_position': barcode_config.barcode_currency_position,
+            }
 
-	@api.model
-	def _get_report_values(self, docids, data=None):
-		barcode_labels_report = self.env['ir.actions.report']._get_report_from_name('bi_dynamic_barcode_labels.product_barcode_labels')
-		barcode_labels = 'form' in data
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        barcode_labels_report = self.env['ir.actions.report']._get_report_from_name('bi_dynamic_barcode_labels.product_barcode_labels')
+        barcode_labels = 'form' in data
         barcode_labels = barcode_labels and 'barcode_labels' in data['form'] or []
         logger.info('Data: ' + str(data['form']))
+        
         if barcode_labels:
-		    barcode_labels = self.env['barcode.product.labels.wiz.line'].browse(barcode_labels)
+            barcode_labels = self.env['barcode.product.labels.wiz.line'].browse(barcode_labels)
+            
 		return {
-			'doc_ids': barcode_labels,
-			'doc_model': barcode_labels_report.model,
-			'docs': barcode_labels,
-			'data': data,
-			'get_barcode_details_info': self._get_barcode_details_info(),
-		}
+            'doc_ids': barcode_labels,
+            'doc_model': barcode_labels_report.model,
+            'docs': barcode_labels,
+            'data': data,
+            'get_barcode_details_info': self._get_barcode_details_info(),
+            }
