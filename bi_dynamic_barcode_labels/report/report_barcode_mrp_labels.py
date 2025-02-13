82bd97481d44143b4559b08a1206d94e4b437ef9
Adam O'Connor <aoconnor@quickbeamllc.com>
2025-02-13 14:29:08 -0800

    attempting to fix report identification

--- bi_dynamic_barcode_labels/report/report_barcode_mrp_labels.py
+++ bi_dynamic_barcode_labels/report/report_barcode_mrp_labels.py
@@ -21,7 +21,7 @@ class DynamicBarcodeMRPLabelsParser(models.AbstractModel):
 
 	@api.model
 	def _get_report_values(self, docids, data=None):
-		barcode_labels_report = self.env['ir.actions.report']._get_report_from_name('bi_dynamic_barcode_labels.mrp_dynamic_barcode_labels')
+		barcode_labels_report = self.env['ir.actions.report']._get_report_from_name('bi_dynamic_barcode_labels.mrp_dynamic_barcode_labels_id')
 		barcode_labels = data['form']['barcode_labels']
 		barcode_labels = self.env['barcode.mrp.labels.wiz.line'].browse(barcode_labels)
 		return {
