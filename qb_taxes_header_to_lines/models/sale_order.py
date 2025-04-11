56cd26563689ecf34cae4c5dc31be33f34d7fd3c
Adam O'Connor <aoconnor@quickbeamllc.com>
2025-04-11 08:56:01 -0700

    Taxes header to order lines customization to not effect Freight lines

--- qb_taxes_header_to_lines/models/sale_order.py
+++ qb_taxes_header_to_lines/models/sale_order.py
@@ -11,4 +11,6 @@ class SaleOrder(models.Model):
             header_taxes = sale.taxes and sale.taxes.ids or []
             if header_taxes:
                 for line in sale.order_line:
-                    line.tax_id = [(6, 0, header_taxes)]
+                    product = line.product_id
+                    if product and product.default_code != 'Freight':
+                        line.tax_id = [(6, 0, header_taxes)]
