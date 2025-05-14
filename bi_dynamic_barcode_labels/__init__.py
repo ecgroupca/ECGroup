3e7ab6b5dc524d16d6c0ad19f8309f9141c497ea
Adam O'Connor <aoconnor@quickbeamllc.com>
2025-05-14 12:23:55 -0700

    small update to trigger rebuild to use current code

--- bi_dynamic_barcode_labels/__init__.py
+++ bi_dynamic_barcode_labels/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+# Part of BrowseInfo. Water. See LICENSE file for full copyright and licensing details.
 
 from . import report
 from . import wizard
